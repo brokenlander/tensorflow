1372703006b4e1cecc999f672da538706e5d855b
brokenlander <94259413+brokenlander@users.noreply.github.com>
2023-01-24 15:33:10 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -27,6 +27,27 @@ def load_and_prep_image(filename, img_shape=224, scale=True):
   else:
     return img
 
+def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
+  """
+  Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+  
+  Parameters
+  ---------
+  timesteps : array of timesteps
+  values : array of values across time
+  format : style of plot, default "."
+  start : where to start the plot (setting a value will index from start of timesteps & values)
+  end : where to end the plot (setting a value will index from end of timesteps & values)
+  label : label to show on plot of values
+  """
+  # Plot the series
+  plt.plot(timesteps[start:end], values[start:end], format, label=label)
+  plt.xlabel("Time")
+  plt.ylabel("BTC Price")
+  if label:
+    plt.legend(fontsize=14) # make label bigger
+  plt.grid(True)
+
 # Note: The following confusion matrix code is a remix of Scikit-Learn's 
 # plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
 import itertools
